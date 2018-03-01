baacb2b87383a3045796db0c9061292a046fca69
Veena0803 <iyerveena08@gmail.com>
2018-02-28 23:10:35 -0500

    Lecture: Arrays

--- script.js
+++ script.js
@@ -215,3 +215,26 @@ if (x === 5) {
     //do soemthing
 }
 */
+
+// Lecture: Arrays
+/*
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+names[1] = 'Ben';
+console.log(names);
+
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+console.log(john);
+
+if (john.indexOf('teacher') === -1) {
+    console.log('John is NOT a teacher.');
+}
+*/
