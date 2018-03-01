c247ed87171ee9af0479708f4e243614ee5e2583
Veena0803 <iyerveena08@gmail.com>
2018-02-28 23:07:25 -0500

    Add coding challenge 1 question

--- script.js
+++ script.js
@@ -129,9 +129,18 @@ switch (job) {
 }
 */
 
+// CODING CHALLENGE 1
 
+/*
+John and a friend invented a simple game where the player with the highest value of his height (in centimeters) plus five times his age wins (what a silly game :)
 
+1. Create variables for the heights and ages of two friends and assign them some values
+2. Calculate their scores
+3. Decide who wins and print the winner to the console. Include the score in the string that you output to the console. Don't forget that there can be a draw (both players with the same score).
 
+4. EXTRA: Add a third player and now decide who wins. Hint: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+
+//SOLUTION:
 
 var ageJohn = 15;
 var ageMark = 15;
@@ -155,4 +164,5 @@ else if(totalJohn < totalMark && totalRoy < totalMark)
 else if(totalRoy > totalJohn && totalRoy > totalMark)
     console.log('Roy is the winner with a score of' + ' ' + totalRoy);
 else
-    console.log('All are the winners');
+    console.log('All are the winners'); 
+*/
