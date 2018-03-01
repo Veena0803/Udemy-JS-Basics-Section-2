3b38c2b82c3f0990e45440584ad6e28ac3dea77d
Veena0803 <iyerveena08@gmail.com>
2018-02-28 23:03:57 -0500

    Lecture: if/else statements

--- script.js
+++ script.js
@@ -68,6 +68,35 @@ console.log(ageJohn);
 console.log(ageMark);
 */
 
+// Lecture: if/else statements
+/*
+var name = 'John';
+var age = 26;
+var isMarried = 'yes';
+
+if (isMarried === 'yes') {
+    console.log(name + ' is married!');
+} else {
+    console.log(name + ' will hopefully marry soon :)');
+}
+
+isMarried = true;
+
+if(isMarried) {
+    console.log('YES!');
+} else {
+    console.log('NO!');
+}
+
+if(isMarried) {
+    console.log('YES!');
+}
+
+if (23 === "23") {
+    console.log('Something to print...');
+}
+*/
+
 
 
 
